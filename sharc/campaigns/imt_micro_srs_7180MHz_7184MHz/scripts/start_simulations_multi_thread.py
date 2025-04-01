d1f8c22222b2b148f49c0c10153a64a49d44e0cc
Juliana Garçoni dos Santos <js.garconi@gmail.com>
2025-04-01 10:42:39 -0300

    Update SRS input parameters

--- sharc/campaigns/imt_micro_srs_7180MHz_7184MHz/scripts/start_simulations_multi_thread.py
+++ sharc/campaigns/imt_micro_srs_7180MHz_7184MHz/scripts/start_simulations_multi_thread.py
@@ -2,7 +2,7 @@ from sharc.run_multiple_campaigns_mut_thread import run_campaign
 
 # Set the campaign name
 # The name of the campaign to run. This should match the name of the campaign directory.
-name_campaign = "imt_micro_mss_7900MHz_8025MHz"
+name_campaign = "imt_micro_srs_7180MHz_7184MHz"
 
 # Run the campaigns
 # This function will execute the campaign with the given name.
